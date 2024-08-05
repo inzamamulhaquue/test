3e197d2380995a17354e2d0aad4ab28a4a9848f6
Inzamamul <you@example.com>
2024-08-05 18:04:23 +0530

    middleware v

--- server.js
+++ server.js
@@ -10,14 +10,18 @@ connectDatabase();
 
 const port = process.env.port || 6006;
 
+// Middleware 
+const logrequest = (req, res, next) =>{
+    console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
+    next();
+}
+
 const userRoutes = require('./routes/userRoute');
 const candidateRoutes = require('./routes/candidateRoute');
 
 // use route
-app.use('/user', userRoutes);
+app.use('/user', logrequest,  userRoutes);
 app.use('/candidate', candidateRoutes);
-// add comment try to hit github
-
 
 app.listen(port, () =>{
     console.log(`server is running on https://localhost:${port}`);
